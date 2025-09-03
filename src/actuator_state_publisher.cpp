3ea292f75def2048ba6961bfd197b46e29184c32
Błażej Szargut <blazejszargut@gmail.com>
2025-09-03 14:06:39 +0000

    Fix initialization order by moving rclcpp::init call to the beginning of main

--- src/actuator_state_publisher.cpp
+++ src/actuator_state_publisher.cpp
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[])
 {
+	rclcpp::init(argc, argv);
 	auto candle = std::make_shared<mab::Candle>(mab::CAN_BAUD_8M, true);
 	auto ids = candle->ping();
 	for (auto &id : ids)
@@ -21,7 +22,6 @@ int main(int argc, char *argv[])
 
 	candle->begin();
 
-	rclcpp::init(argc, argv);
 	auto state_pub = std::make_shared<ActuatorStatePublisher>(candle);
 
 	rclcpp::spin(state_pub);
